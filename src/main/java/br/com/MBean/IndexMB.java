9f820005e676b204f8de0a898e0683b09438b17d
tarcisobandeira <tarcisojb@gmail.com>
2018-09-19 10:42:53 -0300

    telas em geral e mbean

--- src/main/java/br/com/MBean/IndexMB.java
+++ src/main/java/br/com/MBean/IndexMB.java
@@ -1,5 +1,14 @@
 package br.com.MBean;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
 public class IndexMB {
+	
+	public String troca() {
+		return "telaLogin?faces-redirect=true";
+	}
 	
 }
