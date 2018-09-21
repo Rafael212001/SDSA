304b9ca951c70ae29998064974d8805cc150c426
Romulopires <romlopires1988@gmail.com>
2018-09-21 12:06:18 -0300

    romulo DAO

--- src/main/java/br/com/DAO/carga_horaria_restanteDAO.java
+++ src/main/java/br/com/DAO/carga_horaria_restanteDAO.java
@@ -2,11 +2,13 @@ package br.com.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import br.com.entities.Carga_horaria_restante;
+import br.com.entities.Coordenador;
 import br.com.entities.Curso;
 import br.com.jdbc.ConnectionDB;
 
@@ -31,16 +33,51 @@ public class carga_horaria_restanteDAO {
 			}
 
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
+		return false;
+
+	}
+
+	public List<Carga_horaria_restante> listarTodos() {
+		List<Carga_horaria_restante> list = new ArrayList<Carga_horaria_restante>();
+		String sql = "SELECT * FROM carga_horaria_restante";
+
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				Carga_horaria_restante c = new Carga_horaria_restante(rs.getInt("id"), rs.getInt("tempo"),
+						rs.getInt("id_disciplina"), rs.getInt("id_turma"));
+
+				list.add(c);
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public boolean excluir(Integer id) {
+		String sql = "DELETE * FROM carga_horaria_restante " + "WHERE id = ? ";
+		
+
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, id);
+
+			if (ps.executeUpdate() > 0) {
+				return true;
+			}
+		} catch (SQLException e) {
+			
 			e.printStackTrace();
 		}
 		return false;
 
 	}
-//	public List<Carga_horaria_restante> listarTodos() {
-//		List<Carga_horaria_restante> list = new ArrayList<Carga_horaria_restante>();
-//		String sql = "SELECT * FROM carga_horaria_restante";
-//		
-//		PreparedStatement ps = con.prepareStatement(sql);
-//	}
+
 }
--- src/main/java/br/com/DAO/cursoDAO.java
+++ src/main/java/br/com/DAO/cursoDAO.java
@@ -1,6 +1,5 @@
 package br.com.DAO;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,69 +11,67 @@ import br.com.entities.Curso;
 import br.com.jdbc.ConnectionDB;
 
 public class cursoDAO {
-	
+
 	Connection con;
-	
+
 	public cursoDAO() {
-		
+
 		con = ConnectionDB.getConnection();
 	}
-	
+
 	public boolean inserir(Curso c) {
-		
-		String sql = "INSERT INTO curso(nome,qtd_semestre)"
-				+ "VALUES (?,?)";
-		
+
+		String sql = "INSERT INTO curso(nome,qtd_semestre)" + "VALUES (?,?)";
+
 		try {
 			PreparedStatement ps = con.prepareStatement(sql);
 			ps.setString(1, c.getNome());
 			ps.setInt(2, c.getQtd_semestre());
-			
-			if(ps.executeUpdate() > 0 ) {
+
+			if (ps.executeUpdate() > 0) {
 				return true;
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return false;
-		
+
 	}
-	
-	public List<Curso> listarTodos(){
-		List<Curso>list = new ArrayList<Curso>();
+
+	public List<Curso> listarTodos() {
+		List<Curso> list = new ArrayList<Curso>();
 		String sql = "SELECT * FROM curso";
-		
+
 		try {
 			PreparedStatement ps = con.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
-			
-			while(rs.next()) {
-				Curso c = new Curso(rs.getInt("id"),rs.getString("nome"),rs.getInt("qtd_semestre"));
+
+			while (rs.next()) {
+				Curso c = new Curso(rs.getInt("id"), rs.getString("nome"), rs.getInt("qtd_semestre"));
 				list.add(c);
 			}
-			
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return list;
 	}
-	
+
 	public boolean excluir(Integer id) {
 		String sql = "DELETE * FROM  curso" + "WHERE id = ?";
-		
+
 		try {
 			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setInt(1,id);
-			
-			if(ps.executeUpdate() > 0 ) {
+			ps.setInt(1, id);
+
+			if (ps.executeUpdate() > 0) {
 				return true;
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -83,4 +80,3 @@ public class cursoDAO {
 	}
 
 }
-
