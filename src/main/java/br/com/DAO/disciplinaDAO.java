2c93a00ab750174c1fcb0332e8fb3c2c01f26a86
Rafael212001 <rafael_ferreira2013@hotmail.com.br>
2018-09-21 04:36:54 -0300

    to aqui

--- src/main/java/br/com/DAO/disciplinaDAO.java
+++ src/main/java/br/com/DAO/disciplinaDAO.java
@@ -1,6 +1,22 @@
 package br.com.DAO;
 
+import java.sql.Connection;
+
+import br.com.entities.Disciplina;
+import br.com.jdbc.ConnectionDB;
+
 public class disciplinaDAO {
+	
+	Connection con;
+	
+	public disciplinaDAO() {
+		con = ConnectionDB.getConnection();
+	}
+	
+	public boolean inserir(Disciplina d) {
+		String sql = "INSERT INTO disciplina(?,?)";
+		
+	}
 	
 
 }
