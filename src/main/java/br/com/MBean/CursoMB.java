1f73468af970a1edcb7faefb8d084f045236c442
Rafael212001 <rafael_ferreira2013@hotmail.com.br>
2018-09-25 04:50:19 -0300

    tgtyhg6

--- src/main/java/br/com/MBean/CursoMB.java
+++ src/main/java/br/com/MBean/CursoMB.java
@@ -13,10 +13,44 @@ public class CursoMB {
 	cursoDAO cDAO;
 	List<Curso> cuu;
 
-public CursoMB(){
-	cur = new Curso();
+	public CursoMB() {
+		cur = new Curso();
+		cDAO = new cursoDAO();
+		cuu = cDAO.listarTodos();
+	}
+	
+	public void criarCurso() {
+		
+	}
 	
 	
 	
-}
+	
+	
+
+	public Curso getCur() {
+		return cur;
+	}
+
+	public void setCur(Curso cur) {
+		this.cur = cur;
+	}
+
+	public cursoDAO getcDAO() {
+		return cDAO;
+	}
+
+	public void setcDAO(cursoDAO cDAO) {
+		this.cDAO = cDAO;
+	}
+
+	public List<Curso> getCuu() {
+		return cuu;
+	}
+
+	public void setCuu(List<Curso> cuu) {
+		this.cuu = cuu;
+	}
+	
+	
 }
