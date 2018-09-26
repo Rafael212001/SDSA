5095f6eea842c7bd8c4bde4ec7e290b840361462
Rafael212001 <rafael_ferreira2013@hotmail.com.br>
2018-09-26 04:41:53 -0300

    inicio_curso_mbean

--- src/main/java/br/com/MBean/SalasMBean.java
+++ src/main/java/br/com/MBean/SalasMBean.java
@@ -1,13 +1,24 @@
 package br.com.MBean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+
+import br.com.DAO.salaDAO;
+import br.com.entities.Salas;
 
 @ManagedBean
 @RequestScoped
 public class SalasMBean {
 	
+	Salas sal;
+	List<Salas> Lsal;
+	salaDAO sDAO;
 	
+	public SalasMBean() {
+		
+	}
 	
 
 }
