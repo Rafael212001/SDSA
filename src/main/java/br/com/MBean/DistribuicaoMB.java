25cdfdea86f40ac8aee1ef39591a3d0383dabf00
Rafael212001 <rafael_ferreira2013@hotmail.com.br>
2018-10-02 04:45:42 -0300

    banco e destri

--- src/main/java/br/com/MBean/DistribuicaoMB.java
+++ src/main/java/br/com/MBean/DistribuicaoMB.java
@@ -1,5 +1,61 @@
 package br.com.MBean;
 
-public class DistribuicaoMB {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.event.DragDropEvent;
+
+import br.com.entities.Aulas;
+
+@ManagedBean
+@ViewScoped
+public class DistribuicaoMB implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	private List<Aulas> aulass;
+	private List<Aulas> drop;
+	private Aulas selecionadas;
+	
+	public void onAulasDropSala1(DragDropEvent dde) {
+		Aulas aulas = ((Aulas) dde.getData());
+		
+		drop.add(aulas);
+		aulass.remove(aulas);
+	}
+	
+	
+	
+	public List<Aulas> getAulas() {
+		return aulass;
+	}
+
+	public void setAulas(List<Aulas> aulas) {
+		this.aulass = aulas;
+	}
+
+	public List<Aulas> getDrop() {
+		return drop;
+	}
+
+	public void setDrop(List<Aulas> drop) {
+		this.drop = drop;
+	}
+
+	public Aulas getSelecionadas() {
+		return selecionadas;
+	}
+
+	public void setSelecionadas(Aulas selecionadas) {
+		this.selecionadas = selecionadas;
+	}
+	
+	
+	
 	
 }
