f21ce19c982c61fd7a2ac19a926fe5d705558e53
Rafael212001 <rafael_ferreira2013@hotmail.com.br>
2018-10-05 04:42:40 -0300

    distribuicao parte 2

--- src/main/java/br/com/DAO/aulasDAO.java
+++ src/main/java/br/com/DAO/aulasDAO.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 
 import br.com.entities.Aulas;
+import br.com.entities.Curso;
 import br.com.jdbc.ConnectionDB;
 
 
@@ -22,15 +23,21 @@ public class aulasDAO {
 	
 	public List<Aulas> listarTodos(){
 		List<Aulas> list = new ArrayList<Aulas>();
-		String sql = "SELECT * FROM aulas ";
+		String sql = " SELECT c.nome as curso, t.nome as turma, d.nome as disciplina, co.nome as colaborador, cr.nome as coordenador" + 
+				"FROM aulas " + 
+				"INNER JOIN cursos c ON (a.id_cursos = c.id)" + 
+				"INNER JOIN turmas t ON (a.id_turmas = t.id)" + 
+				"INNER JOIN disciplinas d ON (a.id_disciplina = d.id)" + 
+				"INNER JOIN colaboradores co ON (a.id_colaborador = co.id)" + 
+				"INNER JOIN coordenadores cr ON (a.id_coordenador = cr.id) ";
 		
 		try {
 			PreparedStatement ps = con.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
 			
 			while(rs.next()) {
-				Aulas a = new Aulas(rs.getInt("id"), rs.getInt("id_cursos"), rs.getInt("id_turmas"), rs.getInt("id_disciplina"), rs.getInt("id_colaborador"), 
-						rs.getInt("id_colaborado"), rs.getInt("id_sala"));
+				Aulas a = new Aulas(rs.getInt("id"), new Curso(null, rs.getString("curso"), null), rs.getInt("id_turmas"), rs.getInt("id_disciplina"), rs.getInt("id_colaborador"), 
+						rs.getInt("id_coordenador"), rs.getInt("id_sala"));
 				list.add(a);
 			}
 			
