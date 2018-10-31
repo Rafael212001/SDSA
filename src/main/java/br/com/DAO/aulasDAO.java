bc2336b09622c952e1a7f5fbf2a90e3778c13f22
Rafael212001 <rafael_ferreira2013@hotmail.com.br>
2018-10-31 05:38:32 -0200

    ddd

--- src/main/java/br/com/DAO/aulasDAO.java
+++ src/main/java/br/com/DAO/aulasDAO.java
@@ -63,10 +63,12 @@ public class aulasDAO {
 	
 	public List <Aulas> listarAulasAlocadas(Integer dia_semana, Integer numero){
 		List<Aulas> list = new ArrayList<Aulas>();
-		String sql = " SELECT  a.*, c.nome as curso, t.nome as turma, d.nome as disciplina FROM aulas a " +
+		String sql = " SELECT  a.*, c.nome as curso, t.nome as turma, d.nome as disciplina, co.nome as professor "
+				+ "FROM aulas a " +
 		" INNER JOIN cursos c ON (a.id_cursos = c.id) " +
 		" INNER JOIN turmas t ON (a.id_turmas = t.id) " +
 		" INNER JOIN disciplinas d ON (a.id_disciplina = d.id) " +
+		"INNER JOIN colaboradores co ON (a.id_colaborador = co.id) " +
 		" WHERE id_sala = ? AND dia_semana = ? ";
 		
 		try {
@@ -82,7 +84,7 @@ public class aulasDAO {
 						new Curso(null, rs.getString("curso"), null), 
 						new Turma(null, rs.getString("turma"),null, null, null, null, null ), 
 						new Disciplina(null, rs.getString("disciplina"), null, null,null),
-						null, 
+						new Professor(null, rs.getString("professor"), null, null, null, null, null, null), 
 						null, 
 						rs.getInt("id_sala"),rs.getInt("dia_semana"),rs.getInt("carga"));
 				list.add(a);
